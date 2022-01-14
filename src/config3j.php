1453ad7c5cdfbef24664a6bede385f4c699a9983
Jitendra Pal <55188975+threej-in@users.noreply.github.com>
2022-01-14 15:17:54 +0530

    create main configuration file

---
+++ src/config3j.php
@@ -0,0 +1,27 @@
+<?php
+    define('ROOTDIR',str_replace('\\','/',__DIR__).'/');
+    $basedir = explode('/',$_SERVER['REQUEST_URI'])[1];
+    define(
+        'HOMEURI',
+        $_SERVER['REQUEST_SCHEME'] .'://'. 
+        $_SERVER['SERVER_NAME'].':' .
+        $_SERVER['SERVER_PORT'] . '/' .
+        $basedir . '/' . preg_replace('#.*/'.$basedir.'/#','',ROOTDIR)
+    );
+
+    define('DEBUGGING',true);
+
+    //database details
+    //specify server name
+    $SERVER = $_SERVER['SERVER_NAME'];
+    //specify database name
+    $DATABASE = 'bbvs';
+    //Enter db username
+    $DBUSER = 'root';
+    //Enter db password
+    $DBPASS = '';
+
+    //salt
+    $SALT = 'replaceThisWithRandomlyGeneratedString';
+    
+?>
